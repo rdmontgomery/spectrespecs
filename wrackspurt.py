8f0787f53da734ffcfb3847302d1276de7fae8a0
rdmontgomery <rick.montgomery@gmail.com>
2018-06-27 04:31:57 +0000

    Revert to faster capture and simplify output

--- wrackspurt.py
+++ wrackspurt.py
@@ -15,21 +15,15 @@ def instantiate_camera(resolution=(640, 480), framerate=24):
 
 
 def get_frame(camera, stream):
-    camera.capture(stream, format='jpeg')
-    data = np.fromstring(stream.getvalue(), dtype=np.uint8)
-    frame = cv2.imdecode(data, 1)
-    cv2.flip(frame, 1, frame)
+    camera.capture(stream, 'bgr', use_video_port=True)
+    grayscale = cv2.cvtColor(stream.array, cv2.COLOR_BGR2GRAY)
 
-    grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+    # Process image to make points of light more distinct as circles
     cv2.equalizeHist(grayscale)
-
     grayscale = cv2.GaussianBlur(grayscale, (9, 9), 1.5)
-
     dilation_params = (5, 5)
     dilate_kernel = np.ones(dilation_params, np.uint8)
     grayscale = cv2.dilate(grayscale, dilate_kernel, iterations=1)
-
     clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
     grayscale = clahe.apply(grayscale)
 
@@ -37,8 +31,6 @@ def get_frame(camera, stream):
 
 
 def findWand(grayscale):
-    output = grayscale.copy()
-
     coords = cv2.HoughCircles(grayscale,
                               cv2.HOUGH_GRADIENT,
                               dp=3,
@@ -48,15 +40,13 @@ def findWand(grayscale):
                               minRadius=4,
                               maxRadius=15)
 
-    circles = coords
-    if circles is not None:
-        circles = np.round(circles[0, :]).astype('int')
-
-        for (x, y, r) in circles:
-            cv2.circle(output, (x, y), r, (128, 0, 0), 2)
-            cv2.rectangle(output, (x-1, y-1), (x+1, y+1), (0, 0, 0), -1)
-
     if coords is not None:
+        # Draw center and circle around found points of light
+        circles = np.round(coords[0, :]).astype('int')
+        for (x, y, r) in circles:
+            cv2.circle(grayscale, (x, y), r, (128, 0, 0), 2)
+            cv2.rectangle(grayscale, (x-1, y-1), (x+1, y+1), (0, 0, 0), -1)
+
         # The following are necessary for calcOpticalFlowPyrLK
         # Coordinates need shape (n, 1, 2), where n is the number of points
         coords.shape = (coords.shape[1], 1, coords.shape[2])
@@ -64,7 +54,7 @@ def findWand(grayscale):
         coords = coords[:, :, :2]
         coords = coords.astype(np.float32)
 
-    return output, coords
+    return coords
 
 
 def trackWand(prev_frame, next_frame, old_points):
@@ -84,28 +74,27 @@ def trackWand(prev_frame, next_frame, old_points):
     # Select points with a good status
     valid_old = old_points[status==1]
     valid_new = new_points[status==1]
-
-    for i, (new, old) in enumerate(zip(valid_new, valid_old)):
-        a, b = new.ravel()
-        c, d = old.ravel()
-        mask = cv2.line(mask, (a, b), (c, d), (0,0,0), 2)
-        frame = cv2.circle(next_frame, (a, b), 5, (255,0,0), -1)
-    img = cv2.add(frame, mask)
+    if len(valid_new) > 0:
+        for i, (new, old) in enumerate(zip(valid_new, valid_old)):
+            a, b = new.ravel()
+            c, d = old.ravel()
+            cv2.line(next_frame, (a, b), (c, d), (128,0,0), 2)
+            cv2.circle(next_frame, (a, b), 5, (255,0,0), -1)
 
 
 def main():
     camera = instantiate_camera()
     prev_frame = None
-    with io.BytesIO() as stream:
+    with PiRGBArray(camera) as stream:
         while cv2.waitKey(1) & 0xFF != ord('q'):
             next_frame = get_frame(camera, stream)
-            output, old_points = findWand(next_frame)
+            old_points = findWand(next_frame)
 
             if prev_frame is not None and old_points is not None:
                 trackWand(prev_frame, next_frame, old_points)
             prev_frame = next_frame
 
-            cv2.imshow('frame', np.hstack([next_frame, output]))
+            cv2.imshow('frame', np.hstack([next_frame]))
             stream.truncate()
             stream.seek(0)
 
