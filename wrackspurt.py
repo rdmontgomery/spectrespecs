fa88e883ee43df3ee4c0d10f0e0920d5c53f5c3b
rdmontgomery <rick.montgomery@gmail.com>
2018-06-27 05:20:07 +0000

    Track simple movements and append directions to marauders_map

--- wrackspurt.py
+++ wrackspurt.py
@@ -57,41 +57,71 @@ def findWand(grayscale):
     return coords
 
 
-def trackWand(prev_frame, next_frame, old_points):
+def trackWand(prev_frame, next_frame, old_points, marauders_map):
     lk_params = dict(winSize=(15, 15),
                      maxLevel=2,
                      criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT,
                                10,
                                0.03))
-    mask = np.zeros_like(prev_frame)
-
     new_points, status, err = cv2.calcOpticalFlowPyrLK(prev_frame,
                                                        next_frame,
                                                        old_points,
                                                        None,
                                                         **lk_params)
-    print(new_points, status, err)
-    # Select points with a good status
+    # Select points with a good status and small error
     valid_old = old_points[status==1]
     valid_new = new_points[status==1]
+    valid_old = old_points[err<5]
+    valid_new = new_points[err<5]
+
     if len(valid_new) > 0:
-        for i, (new, old) in enumerate(zip(valid_new, valid_old)):
-            a, b = new.ravel()
-            c, d = old.ravel()
-            cv2.line(next_frame, (a, b), (c, d), (128,0,0), 2)
-            cv2.circle(next_frame, (a, b), 5, (255,0,0), -1)
+        for i, (old, new) in enumerate(zip(valid_old, valid_new)):
+            x0, y0 = old.ravel()
+            x1, y1 = new.ravel()
+            coord_change = (x0, y0, x1, y1)
+            # Track movement on highly-rated points(?) 
+            if i < 5:
+                dist = np.hypot(x1 - x0, y1 - y0)
+                if dist < 80:
+                    cv2.line(next_frame, (x0, y0), (x1, y1), (128,0,0), 2)
+                    cv2.circle(next_frame, (x1, y1), 5, (255,0,0), -1)
+                    marauders_map = manage_mischief(marauders_map, coord_change, i)
+
+    return marauders_map
+
+
+def manage_mischief(marauders_map, coord_change, i):
+    (x0, y0, x1, y1) = coord_change
+
+    if (x1 < x0 - 5) & (abs(y1 - y0) < 1):
+        marauders_map[i].append('left')
+    elif (x1 > x1 + 5) & (abs(y1 - y0) < 1):
+        marauders_map[i].append('right')
+    elif (y1 < y0 - 5) & (abs(x1 - x0) < 1):
+        marauders_map[i].append('down')
+    elif (y1 > y0 + 5) & (abs(x1 - x0) < 1):
+        marauders_map[i].append('up')
+
+    direction = ''.join(map(str, marauders_map[i]))
+    print(i, direction)
+
+    return marauders_map
 
 
 def main():
     camera = instantiate_camera()
     prev_frame = None
+
+    # marauders_map contains the movement of the wand over time
+    marauders_map = [[] for _ in range (20)]
+
     with PiRGBArray(camera) as stream:
         while cv2.waitKey(1) & 0xFF != ord('q'):
             next_frame = get_frame(camera, stream)
             old_points = findWand(next_frame)
 
             if prev_frame is not None and old_points is not None:
-                trackWand(prev_frame, next_frame, old_points)
+                marauders_map = trackWand(prev_frame, next_frame, old_points, marauders_map)
             prev_frame = next_frame
 
             cv2.imshow('frame', np.hstack([next_frame]))
