64404bd129d30ccf0df21b68d23c40a00f28d6ab
rdmontgomery <rick.montgomery@gmail.com>
2018-06-27 04:08:42 +0000

    PyrLK needs points to be shape (n, 1, 2) and type float32
    Tracking is working right now but I still need to get a better idea of which images should be drawn on which frames.

--- wrackspurt.py
+++ wrackspurt.py
@@ -47,7 +47,7 @@ def findWand(grayscale):
                               param2=8,
                               minRadius=4,
                               maxRadius=15)
-    
+
     circles = coords
     if circles is not None:
         circles = np.round(circles[0, :]).astype('int')
@@ -56,42 +56,59 @@ def findWand(grayscale):
             cv2.circle(output, (x, y), r, (128, 0, 0), 2)
             cv2.rectangle(output, (x-1, y-1), (x+1, y+1), (0, 0, 0), -1)
 
+    if coords is not None:
+        # The following are necessary for calcOpticalFlowPyrLK
+        # Coordinates need shape (n, 1, 2), where n is the number of points
+        n = coords.shape[1]
+        coords.shape = (n, 1, coords.shape[2])
+        # Select just the (x, y) coords, removing the radius
+        coords = coords[:, :, :2]
+        # Cast as float32
+        coords = coords.astype(np.float32)
+
     # threading.Timer(1, findWand, [grayscale]).start()
     return output, coords
 
 
-def main():
+def trackWand(prev_frame, next_frame, old_points):
     lk_params = dict(winSize=(15, 15),
                      maxLevel=2,
                      criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT,
                                10,
                                0.03))
+    mask = np.zeros_like(prev_frame)
 
+    new_points, status, err = cv2.calcOpticalFlowPyrLK(prev_frame,
+                                                       next_frame,
+                                                       old_points,
+                                                       None,
+                                                        **lk_params)
+    print(new_points, status, err)
+    # Select points with a good status
+    valid_old = old_points[status==1]
+    valid_new = new_points[status==1]
+
+    for i, (new, old) in enumerate(zip(valid_new, valid_old)):
+        a, b = new.ravel()
+        c, d = old.ravel()
+        mask = cv2.line(mask, (a, b), (c, d), (0,0,0), 2)
+        frame = cv2.circle(next_frame, (a, b), 5, (255,0,0), -1)
+    img = cv2.add(frame, mask)
+
+
+def main():
     camera = instantiate_camera()
     prev_frame = None
     with io.BytesIO() as stream:
         while cv2.waitKey(1) & 0xFF != ord('q'):
             next_frame = get_frame(camera, stream)
             output, old_points = findWand(next_frame)
-            if old_points is not None:
-                old_points.shape = (old_points.shape[1], 1, old_points.shape[2])
-                #print(old_points)
-                old_points = old_points[:, :, 0:2][:, 0]
-                #print(old_points)
-                #print(old_points[:, 0])
 
-            if prev_frame is not None:
-                pass
-                # print(prev_frame)
-                new_points, status, err = cv2.calcOpticalFlowPyrLK(prev_frame,
-                                                                   next_frame,
-                                                                   old_points,
-                                                                   None,
-                                                                    **lk_params)
-                print(new_points, status, err)
+            if prev_frame is not None and old_points is not None:
+                trackWand(prev_frame, next_frame, old_points)
             prev_frame = next_frame
 
-            cv2.imshow('frame', np.hstack([prev_frame, output]))
+            cv2.imshow('frame', np.hstack([next_frame, output]))
             stream.truncate()
             stream.seek(0)
 
